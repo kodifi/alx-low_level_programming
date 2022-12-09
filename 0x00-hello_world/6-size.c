e90e6cd9343b77b94185a86e9c990b3b37948462
kodifi <codifi.jk@gmail.com>
2022-12-08 19:00:38 -0800

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	float f;
 
 	printf("size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
-	printf("size of a int: %lu byte(S)\n", (unsigned long)sizeof(b));
+	printf("size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
 	printf("size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 	printf("size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
 	printf("size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
