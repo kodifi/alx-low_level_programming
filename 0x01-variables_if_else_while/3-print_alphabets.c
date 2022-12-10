dfdf01a1c44c2bf8ef9455628add4fb964c626cb
kodifi <codifi.jk@gmail.com>
2022-12-09 21:10:47 -0800

    lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, and then in uppercase.
+ * main - Prints the alphabet in lowercase, follow by an uppercase.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-        char letter;
+	char letter;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-                putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-        for (letter = 'A'; letter <= 'Z'; letter++)
-                putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
